0baa02c9565eca5a1ca75936569da0ca3fb42f7b
Pedrocazevedo <pedrocarlin2@icloud.com>
2026-03-16 22:36:55 -0300

    perfil-candidato_banner

---
+++ perfil-candidato.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Perfil - Pedro</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php require_once "templates/header.php" ?>
+<header class="topo">
+
+</header>
+<div id="banner-perfil-c">
+    
+
+
+
+</div>
+
+
+
+
+
+<section class="capa"></section>
+
+<section class="perfil">
+<img class="foto-perfil" src="">
+
+<div class="info">
+<h1>Pedro</h1>
+<p class="profissao">Técnico em T.I</p>
+</div>
+</section>
+
+<main class="container">
+
+<aside class="card sobre">
+
+<h2>Sobre</h2>
+
+<p>
+Profissional de Tecnologia da Informação com conhecimento em desenvolvimento web,
+suporte técnico e design gráfico.
+</p>
+
+<h2>Certificados</h2>
+
+<div class="certificado">JavaScript</div>
+<div class="certificado">PHP</div>
+<div class="certificado">Photoshop</div>
+
+<button class="botao">Entrar em contato</button>
+
+</aside>
+
+<section class="card conteudo">
+
+<h2>Experiência</h2>
+
+<p>💻 Desenvolvimento de sistemas web.</p>
+<p>🖥 Manutenção de computadores.</p>
+<p>🎨 Edição de imagens utilizando Photoshop.</p>
+
+<h2>Habilidades</h2>
+
+<p>HTML • CSS • JavaScript • PHP • Photoshop</p>
+
+</section>
+
+</main>
+
+</body>
+</html>
--- templates/header_pedro.php
+++ templates/header_pedro.php
@@ -1,1 +1,12 @@
-<!-- pedro -->
+<!-- pedro -->  
+     <header>
+        <nav>
+            <img src="img/logo.svg" alt="logo" class="logo">
+            <ul class="menu">
+                <li><a href="#MgsFourth">SOBRE</a></li>
+                <li><a href="#suporte">SUPORTE</a></li>
+                <li><a href="cadastro-candidato.php">HOME</a></li>
+            </ul>
+            <a href="login.php" class="user-icon"><i class="fa-solid fa-user"></i></a>
+        </nav>
+    </header>
