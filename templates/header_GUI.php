9b5186250dec2494a5b4a215076d800240885f3b
Guilherme-SQ <gk181796@gmail.com>
2026-03-23 22:26:44 -0300

    add

--- templates/header_GUI.php
+++ templates/header_GUI.php
@@ -1,12 +1,18 @@
-   <!-- KAUÃ -->
+<?php require_once "modal_post.php"; ?>
    <header>
-        <nav>
-            <img src="img/logo.svg" alt="logo" class="logo">
-            <ul class="menu">
-                <li><a href="#MgsFourth">VAGAS DISPONIVEIS</a></li>
-                <li><a href="#suporte">PESQUISAR</a></li>
-                <li><a href="#">VAGAS PREENCHIDAS</a></li>
-            </ul>
-            <a href="login.php" class="user-icon"><i class="fa-solid fa-user"></i></a>
-        </nav>
-    </header>
+       <nav>
+           <img src="img/logo.svg" alt="logo" class="logo">
+           <ul class="menu">
+               <li><a href="#MgsFourth">FILTRO<i class="fa-solid fa-arrow-down"></i></a></li>
+               <li>
+                   <a href="#" data-bs-toggle="modal" data-bs-target="#modalCertificado">
+                       ADICIONAR
+                   </a>
+               </li>
+               <li><a href="#">VAGAS PREENCHIDAS</a></li>
+           </ul>
+           <a href="login.php" class="user-icon"><i class="fa-solid fa-user"></i></a>
+       </nav>
+   </header>
+
+  
