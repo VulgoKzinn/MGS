f13c3259b8740d5b22f7a378ca7d36c93a5468e2
Guilherme-SQ <gk181796@gmail.com>
2026-03-31 21:08:53 -0300

    add suporte

---
+++ suporte.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MGS | Suporte</title>
+    <?php
+    require_once 'assets/templates/head.php';
+    ?>
+</head>
+
+<body id="cadastroCand">
+    <div id="ImgLogon">
+        <a href="index.php"><img src="assets/img/Logomaior.png" alt="Logo"></a>
+    </div>
+    <!-- Formulário -->
+    <main id="CadCand">
+        <form action="" method="post" class="p-4">
+            <h2 class="text-center mb-4">Central de Suporte</h2>
+
+            <!-- Botão -->
+            <div class="text-end">
+                <button type="submit"
+                    class="btn btn-success">Enviar</button>
+            </div>
+        </form>
+    </main>
+
+    <!-- Include JS -->
+    <?php
+    require_once 'assets/templates/js.php';
+    ?>
+
+</html>
+</body>
+
+</html>
