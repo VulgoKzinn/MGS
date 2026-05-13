7e055de2c73c3d593cff802e6915a246dc091eed
Guilherme-SQ <gk181796@gmail.com>
2026-05-13 20:00:38 -0300

    add listando
    ultimo adicionado para o primeiro adicionado

--- match-candidato.php
+++ match-candidato.php
@@ -1,8 +1,8 @@
 <?php
 require_once "backend/includes/funcoes.php";
+session_start();
 validaAcesso();
-$id_nivel = $_SESSION['id_nivel'];
-validaEmpresa($id_nivel);
+validaUsuario();
 
 ?>
 <!DOCTYPE html>
